5c7b7015682982f7ebc2d10a4cbc929771fd64e3
RadjaShiqnals <radjagenta@gmail.com>
2024-10-21 06:38:47 +0700

    Initial commit

---
+++ database/migrations/2024_10_20_232856_create_pembelian_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembelian', function (Blueprint $table) {
+            $table->id('pembelian_id');
+            $table->foreignId('pembelian_user_id')->constrained('users', 'id')->onDelete('cascade');
+            $table->foreignId('pembelian_metode_pembayaran_id')->constrained('metode_pembayaran', 'metode_pembayaran_id')->onDelete('cascade');
+            $table->timestamp('pembelian_tanggal')->nullable(false);
+            $table->integer('pembelian_total_harga')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembelian');
+    }
+};
